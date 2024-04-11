41a9cff2f5b5e97a5c3ca17f1e8ba6ae2f5d4ede
Vignesh Mohan <so@yahoo.com>
2024-04-12 00:34:40 +0530

    Added About page and used templating

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "store/home.html", {})
+    products = Product.objects.all()
+    return render(request, "store/home.html", {"products":products})
+
+
+def about(request):
+    return render(request, "store/about.html", {})
